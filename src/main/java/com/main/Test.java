451123cc2c32a9a330b202c47b2da7003d423892
suisuiee <binnee7@hanmail.net>
2023-05-31 14:26:09 +0900

    commit1,2,3

---
+++ src/main/java/com/main/Test.java
@@ -0,0 +1,16 @@
+package com.main;
+
+public class Test {
+    private static String firstMethod() {
+        return "1";
+    }
+
+    private static String secondMethod() {
+        // 사용자로부터 입력 받는 로직
+        return "2";
+    }
+
+    private static String thirdMethod() {
+        return "3";
+    }
+}
